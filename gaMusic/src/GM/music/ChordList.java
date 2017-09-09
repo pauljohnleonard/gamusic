8191a723e9fc6207bd42fd9683ce530ccb43b6e9
paul <paul@Candy.local>
2017-09-09 10:45:22 +0100

    INitial 2

---
+++ gaMusic/src/GM/music/ChordList.java
@@ -0,0 +1,23 @@
+package GM.music;
+
+
+/**
+ * Write a description of class ChordList here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ChordList extends EventList
+{
+
+    public ChordList(){}
+
+    /*
+    public ChordList(GeneReader r) throws Exception  {
+        int n=(int)r.getLong();
+        for (int i=0;i<n;i++) {
+            add(new Chord(r));
+        }
+    }
+    */
+}
